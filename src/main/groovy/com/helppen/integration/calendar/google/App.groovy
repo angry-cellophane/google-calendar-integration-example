7ecce0985d05f97d0ca4817f5ad080723e6203f0
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2015-11-29 17:08:28 +0300

    Initial commit

---
+++ src/main/groovy/com/helppen/integration/calendar/google/App.groovy
@@ -0,0 +1,11 @@
+package com.helppen.integration.calendar.google
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class App {
+    static void main(String[] args) {
+        def ctx = SpringApplication.run(App.class, args)
+    }
+}
